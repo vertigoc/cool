4a79ccbeae43e5fd4b4fdc455dc671283e951997
vertigoc <90930255+vertigoc@users.noreply.github.com>
2021-12-03 21:04:28 +0000

    Create Autofarm40k25s.lua

---
+++ Games/Madcity/Autofarm40k25s.lua
@@ -0,0 +1,2 @@
+_G.MaxPlayers = 7 -- select the number of players on the server for teleportation
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ThisUsernameWasObfuscatedUsingPSUObf40A/Scripts/main/MadCityAutoRob"))()
