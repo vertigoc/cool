a68652a7ac9a020c87193364dc6c836b5deb0f43
vertigoc <90930255+vertigoc@users.noreply.github.com>
2021-12-03 21:11:13 +0000

    Update HugeCatCodeGen.lua

--- Games/PetSimX/HugeCatCodeGen.lua
+++ Games/PetSimX/HugeCatCodeGen.lua
@@ -1,41 +1,80 @@
-getgenv().Count = 100000 --how much codes to generate and check
+--// localizations
 
-local codes = 'abcdefghijklmnopqrstuvwxyz0123456789'
-if not isfile('working codes.txt') then
-   writefile('working codes.txt', '')
-end
+local rconsoleprint = rconsoleprint;
+local rconsolename = rconsolename;
 
-function generateCode()
-   local code = ''
-   for i = 1, 10 do
-       code = code .. string.split(codes,'')[math.random(1, #string.split(codes, ''))]
-   end
-   return 'pet-' .. code
-end
+local math_random = math.random;
+local coroutine_wrap = coroutine.wrap;
+local task_wait = task.wait;
+local warn = warn;
 
-local howMuch = 0
-local Working = 0
+--// define init variables
 
-rconsolename('Huge Cat Generator | Checked: ' .. howMuch .. '/' .. Count .. ' | Working: ' .. Working)
+local code_count = 100000;
+local characters_list = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+local character_list_length = #characters_list;
 
-for i = 1,Count do
-   local code = generateCode()
-   local request = workspace.__THINGS.__REMOTES:FindFirstChild("redeem merch code"):InvokeServer({code})
-   if request[1] == false then
-       howMuch = howMuch + 1
-       rconsoleprint('@@RED@@')
-       rconsoleprint('[NOT WORKING] ')
-       rconsoleprint('@@WHITE@@')
-       rconsoleprint(code .. '\n')
-       rconsolename('Huge Cat Generator | Checked: ' .. howMuch .. '/' .. Count .. ' | Working: ' .. Working)
-   elseif request[1] == true then
-       Working = Working + 1
-       Count = Count + 1
-       rconsoleprint('@@LIGHT_GREEN@@')
-       rconsoleprint('[WORKING] ')
-       rconsoleprint('@@WHITE@@')
-       rconsoleprint(code .. '\n')
-       appendfile('working codes.txt', code .. '\n')
-       rconsolename('Huge Cat Generator | Checked: ' .. howMuch .. '/' .. Count .. ' | Working: ' .. Working)
-   end
-end
+local redeem_remote = workspace.__THINGS.__REMOTES:WaitForChild("redeem merch code");
+
+--// create working codes file
+
+if not isfile("working_codes.txt") then
+   writefile("working_codes.txt", "");
+end;
+
+--// function to generate a random 10 character code
+
+function generate_random_code()
+  local code_string = "";
+ 
+  for index = 1, 10 do
+      code_string = code_string .. characters_list[math_random(1, character_list_length)];
+  end;
+ 
+  return "pet-" .. code_string;
+end;
+
+--// stuff to output debug data to the rconsole
+local checked_amount = 0;
+local working_amount = 0;
+
+local function output_debug_data()
+   rconsolename("Huge Cat Generator | Checked: " .. checked_amount .. "/" .. code_count .. " | Working: " .. working_amount);
+end;
+
+output_debug_data();
+
+--// main bruteforce loop
+
+for index = 1, code_count do
+   local random_code = generate_random_code();
+   local code_redeem = redeem_remote:InvokeServer({random_code});
+   local response_status = code_redeem[1];
+   local response_message = code_redeem[2];
+   
+   warn(response_message); -- if you remove the wait(3) at the end of the loop, this will be "You are trying this too much"
+   
+   checked_amount = checked_amount + 1;
+ 
+   if not response_status then
+       rconsoleprint("@@RED@@");
+       rconsoleprint("[NOT WORKING] ");
+       rconsoleprint("@@WHITE@@");
+       rconsoleprint(random_code .. "\n");
+     
+       output_debug_data();
+   else
+       working_amount = working_amount + 1;
+     
+       rconsoleprint("@@LIGHT_GREEN@@");
+       rconsoleprint("[WORKING] ");
+       rconsoleprint("@@WHITE@@");
+       rconsoleprint(random_code .. "\n");
+     
+       appendfile("working codes.txt", random_code .. "\n");
+     
+       output_debug_data();
+   end;
+   
+   wait(3);
+end;
